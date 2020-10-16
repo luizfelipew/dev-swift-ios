584eb0eadfc56c8ddffac60859173e259f9ebcfa
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-10-16 20:37:15 -0300

    conectando a interface ao código IBAction

--- eggplant-brownie/eggplant-brownie/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    @IBAction func adicionar(_ sender: Any) {
+        print("alimento: --")
     }
-
-
+    
 }
 
