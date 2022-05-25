64d62209748345b4d06549b4dac76bdd6b5ef2b6
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-25 08:52:28 -0300

    Criando tableview e manipulando as células

--- eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
@@ -11,13 +11,37 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
-var delegate: AdicionaRefeicaoDelegate?
+    // MARK: - Atributos
+    
+    var delegate: AdicionaRefeicaoDelegate?
+    var itens: [String] = ["Molho de tomate", "Queijo", "Molho apimentado", "Manjericão"]
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let linhaDaTabela = indexPath.row
+        let item = itens[linhaDaTabela]
+        
+        celula.textLabel?.text = item
+        
+        return celula
+    }
 
+    // MARK: - IBOutlet
+    
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet var felicidadeTextField: UITextField?
    
+    // MARK: - IBAction
+    
     @IBAction func adicionar(_ sender: Any) {
         
         guard let nomeDaRefeicao = nomeTextField?.text else {
