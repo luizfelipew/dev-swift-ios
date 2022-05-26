9911e0dcfe9380f44fb04c4b3d5323f34d0e5a1d
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-25 21:43:52 -0300

    múltiplos inicializadores valores padrão de parâmetro e mais boas práticas

--- eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
@@ -11,7 +11,7 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // MARK: - Atributos
     
@@ -34,7 +34,19 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         return celula
     }
-
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let celula = tableView.cellForRow(at: indexPath) else { return }
+        
+        if celula.accessoryType == .none {
+            celula.accessoryType = .checkmark
+        } else {
+            celula.accessoryType = .none
+        }
+    }
+    
     // MARK: - IBOutlet
     
     @IBOutlet var nomeTextField: UITextField?
