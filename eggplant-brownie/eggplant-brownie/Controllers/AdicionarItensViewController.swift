7d8e3c2389c98e0a6ca22d4211d703b943d1c97f
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-29 12:24:45 -0300

    criando um xib

---
+++ eggplant-brownie/eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AdicionarItensViewController.swift
+//  eggplant-brownie
+//
+//  Created by Luiz Felipe Wendt on 29/05/22.
+//
+
+import UIKit
+
+class AdicionarItensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
