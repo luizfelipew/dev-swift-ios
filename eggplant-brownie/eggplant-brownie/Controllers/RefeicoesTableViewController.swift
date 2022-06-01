7e9b95381f0afc4afa7e75fdd161349c699c221e
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-31 22:48:51 -0300

    eventos com longpress

--- eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -33,7 +33,12 @@ class RefeicoesTableViewController: UITableViewController,
     
     @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
         if gesture.state == .began {
-            print("longPress gesture")
+            let celula = gesture.view as! UITableViewCell
+            guard let indexPath = tableView.indexPath(for: celula) else { return }
+            let refeicao = refeicoes[indexPath.row]
+            
+            print("refeiçãoo \(refeicao.nome)")
+            
         }
         
     }
